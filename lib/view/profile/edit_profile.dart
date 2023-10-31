49ca2ef9d24ed034723e7f5f3b1f3b4b0244819f
nashva <nashvakizhakkayil@gmail.com>
2023-10-31 22:59:44 +0530

    notification page and edit profile page is created

--- lib/view/profile/edit_profile.dart
+++ lib/view/profile/edit_profile.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/constatnts/colors.dart';
 import 'package:expense_tracker/constatnts/custom_widgets/common/sizedbox.dart';
+import 'package:expense_tracker/constatnts/custom_widgets/common/textstyle.dart';
 import 'package:expense_tracker/controller/auth_controller.dart';
 import 'package:expense_tracker/models/auth_model/auth_model.dart';
 import 'package:flutter/material.dart';
@@ -34,55 +35,66 @@ class _EditProfileState extends State<EditProfile> {
   Widget build(BuildContext context) {
     AuthModel user = authController.authBox.getAt(0);
     return Scaffold(
+      backgroundColor: Appcolor.tertiaryColor,
       appBar: AppBar(
-        title: Text('Edit Profile'),
+        iconTheme: const IconThemeData(color: Colors.black, size: 28),
+        backgroundColor: Colors.transparent,
+        title: Text(
+          'Edit Profile',
+          style: screenTitleText(),
+        ),
         elevation: 0,
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.check))],
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Stack(
-                alignment: Alignment.bottomCenter,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: CircleAvatar(
-                      radius: 50,
-                      backgroundColor: Appcolor.tertiaryColor,
-                      child: Image.asset('images/user-logo.png'),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              //mainAxisAlignment: MainAxisAlignment.start,
+              //crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: CircleAvatar(
+                        radius: 50,
+                        backgroundColor: Appcolor.tertiaryColor,
+                        child: Image.asset('images/user-logo.png'),
+                      ),
                     ),
-                  ),
-                  const CircleAvatar(
-                    radius: 15,
-                    backgroundColor: Colors.white,
-                    child: Icon(
-                      Icons.add,
-                      color: Appcolor.primaryColor,
+                    const CircleAvatar(
+                      radius: 15,
+                      backgroundColor: Colors.white,
+                      child: Icon(
+                        Icons.add,
+                        color: Appcolor.secondaryColor,
+                      ),
                     ),
+                  ],
+                ),
+                height30,
+                TextField(
+                  controller: nameController,
+                  decoration: const InputDecoration(
+                    enabledBorder: UnderlineInputBorder(),
+                    focusedBorder: UnderlineInputBorder(),
+                    prefixIcon: Icon(Icons.person),
                   ),
-                ],
-              ),
-              height30,
-              TextField(
-                controller: nameController,
-                decoration: InputDecoration(
-                  border: UnderlineInputBorder(),
-                  prefixIcon: Icon(Icons.person),
                 ),
-              ),
-              height30,
-              TextField(
-                controller: emailController,
-                decoration: InputDecoration(
-                  border: UnderlineInputBorder(),
-                  prefixIcon: Icon(Icons.email),
+                height30,
+                TextField(
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    focusedBorder: UnderlineInputBorder(),
+                    prefixIcon: Icon(Icons.email),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
--- lib/view/profile/notification.dart
+++ lib/view/profile/notification.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/constatnts/colors.dart';
 import 'package:flutter/material.dart';
 
 class NotificationPage extends StatelessWidget {
@@ -5,6 +6,26 @@ class NotificationPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Appcolor.tertiaryColor,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Notifications',
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.black),
+      ),
+      body: Column(
+        children: [
+          ListTile(
+            trailing: Switch.adaptive(value: true, onChanged: (value) {}),
+            title: Text('Show notifications'),
+          )
+        ],
+      ),
+    );
   }
 }
